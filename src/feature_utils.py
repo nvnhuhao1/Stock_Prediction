11135d885d432e993c64c5aef0bd5ca6885106ab
nvnhuhao1 <haohao33410@gmail.com>
2026-02-20 14:58:33 +0700

    Update feature_utils.py

--- src/feature_utils.py
+++ src/feature_utils.py
@@ -21,7 +21,7 @@ def extract_features():
     START_DATE = (datetime.date.today() - datetime.timedelta(days=365)).strftime("%Y-%m-%d")
     END_DATE = datetime.date.today().strftime("%Y-%m-%d")
     stk_tickers = ['AMAT_Future', 'INTL', 'RELY']
-    ccy_tickers = ['DEXJPUS', 'DEXUSUK']
+    ccy_tickers = ['DEXJPUS', 'DEXCHUS']
     idx_tickers = ['SP500', 'DJIA', 'VIXCLS']
     
     stk_data = yf.download(stk_tickers, start=START_DATE, end=END_DATE, auto_adjust=False)
@@ -66,6 +66,7 @@ def get_bitcoin_historical_prices(days = 60):
     df['Date'] = pd.to_datetime(df['Timestamp'], unit='ms').dt.normalize()
     df = df[['Date', 'Close Price (USD)']].set_index('Date')
     return df
+
 
 
 
