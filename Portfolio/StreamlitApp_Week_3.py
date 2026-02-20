30c61bafdcaa34283ae1fe6e74b4d04c67008475
nvnhuhao1 <haohao33410@gmail.com>
2026-02-20 00:24:20 -0600

    Refactor imports and update model input keys

--- Portfolio/StreamlitApp_Week_3.py
+++ Portfolio/StreamlitApp_Week_3.py
@@ -60,7 +60,7 @@ MODEL_INFO = {
         "explainer": 'explainer.shap',
         "pipeline": 'finalized_model.tar.gz',
          "keys": ["GOOGL", "IBM", "DEXJPUS", "DEXUSUK", "SP500", "DJIA", "VIXCLS"],
-        "inputs": [{"name": k, "type": "number", "min": -1.0, "max": 1.0, "default": 0.0, "step": 0.01} for k in ["AMAT","INTL","RELY", "DEXJPUS", "DEXUSUK", "SP500", "DJIA", "VIXCLS"]]
+        "inputs": [{"name": k, "type": "number", "min": -1.0, "max": 1.0, "default": 0.0, "step": 0.01} for k in ["AMAT","INTL","RELY", "DEXJPUS", "DEXCHUS", "SP500", "NASDAQCOM", "VIXCLS"]]
 }
 
 def load_pipeline(_session, bucket, key):
@@ -151,6 +151,7 @@ if submitted:
         display_explanation(input_df,session, aws_bucket)
     else:
         st.error(res)
+
 
 
 
