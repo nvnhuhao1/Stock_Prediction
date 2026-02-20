1eb35244f64c06ef0901d7f41f49a73772684697
nvnhuhao1 <haohao33410@gmail.com>
2026-02-20 00:26:27 -0600

    Refactor Streamlit app by reordering imports

--- Portfolio/StreamlitApp_Week_3.py
+++ Portfolio/StreamlitApp_Week_3.py
@@ -140,7 +140,7 @@ with st.form("pred_form"):
 
 if submitted:
 
-    data_row = [user_inputs[k] for k in MODEL_INFO["keys"]]
+    data_row = [user_inputs[k] for k in MODEL_INFO["inputs"]]
     # Prepare data
     base_df = df_features
     input_df = pd.concat([base_df, pd.DataFrame([data_row], columns=base_df.columns)])
@@ -151,6 +151,7 @@ if submitted:
         display_explanation(input_df,session, aws_bucket)
     else:
         st.error(res)
+
 
 
 
