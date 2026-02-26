1615c2243db381b4b99bf52e9f7b7efd95605e7a
nvnhuhao1 <haohao33410@gmail.com>
2026-02-26 13:49:15 -0600

    Reorganize imports and clean up extract_features

--- src/feature_utils.py
+++ src/feature_utils.py
@@ -31,10 +31,10 @@ def extract_features():
 
     #Y = np.log(stk_data.loc[:, ('Adj Close', 'MSFT')]).diff(return_period).shift(-return_period)
     #Y.name = Y.name[-1]+'_Future'
-    Y = np.log(stk_data.loc[:, ('Adj Close', 'APPL')]
+    Y = stk_data.loc[:, ('Adj Close', 'APPL')]
     Y.name = 'APPL'
 
-    X = np.log(stk_data.loc[:, ('Adj Close', 'MPWR')]
+    X = stk_data.loc[:, ('Adj Close', 'MPWR')]
     X.name = 'MPWR'
     
     #X1 = np.log(stk_data.loc[:, ('Adj Close', ('GOOGL', 'IBM'))]).diff(return_period)
@@ -71,5 +71,6 @@ def get_bitcoin_historical_prices(days = 60):
     df['Date'] = pd.to_datetime(df['Timestamp'], unit='ms').dt.normalize()
     df = df[['Date', 'Close Price (USD)']].set_index('Date')
     return df
+
 
 
