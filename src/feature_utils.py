84bdfb4d8ab747f8ebb44e8281f90addfee2cd60
nvnhuhao1 <haohao33410@gmail.com>
2026-04-09 14:33:53 -0500

    Update feature_utils.py

--- src/feature_utils.py
+++ src/feature_utils.py
@@ -95,9 +95,9 @@ def convert_input_pca_regression(request_body, request_content_type):
 
     dataset = pd.read_csv(file_path,index_col=0)
 
-    target = 'MSFT'
+    target = 'GOOGL'
 
-    option = 2
+    option = 1
 
     if option == 2:
 
@@ -125,10 +125,10 @@ def convert_input_pca_regression(request_body, request_content_type):
 
         return_period = 5
 
-        SP500_1 = 'IBM_CR_Cum'
-        IBM_CR_Cum = json.loads(request_body)[SP500_1]
-        SP500_2 = 'NVDA_CR_Cum'
-        NVDA_CR_Cum = json.loads(request_body)[SP500_2]
+        SP500_1 = 'OXY_CR_Cum'
+        OXY_CR_Cum = json.loads(request_body)[SP500_1]
+        SP500_2 = 'FANG_CR_Cum'
+        FANG_CR_Cum = json.loads(request_body)[SP500_2]
 
         X = np.log(dataset.drop([target],axis=1)).diff(return_period)
         X = np.exp(X).cumsum()
@@ -136,15 +136,15 @@ def convert_input_pca_regression(request_body, request_content_type):
         
         # Calculate the distance
         distances = np.sqrt(
-            (X[SP500_1] - IBM_CR_Cum)**2 + 
-            (X[SP500_2] - NVDA_CR_Cum)**2
+            (X[SP500_1] - OXY_CR_Cum)**2 + 
+            (X[SP500_2] - FANG_CR_Cum)**2
         )
         
         closest_index = distances.idxmin()
         closest_row = X.loc[[closest_index]]
     
-        closest_row[SP500_1] = IBM_CR_Cum
-        closest_row[SP500_2] = NVDA_CR_Cum
+        closest_row[SP500_1] = OXY_CR_Cum
+        closest_row[SP500_2] = FANG_CR_Cum
     
         return closest_row
     
