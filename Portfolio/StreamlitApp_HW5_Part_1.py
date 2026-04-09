427f98c57aa1d7646bddd497a9d9f0fff0a2c0a1
nvnhuhao1 <haohao33410@gmail.com>
2026-04-09 15:10:18 -0500

    Update StreamlitApp_HW5_Part_1.py

--- Portfolio/StreamlitApp_HW5_Part_1.py
+++ Portfolio/StreamlitApp_HW5_Part_1.py
@@ -123,10 +123,10 @@ def display_explanation(input_df, session, aws_bucket):
 
     st.subheader("🔍 Decision Transparency (SHAP)")
     fig, ax = plt.subplots(figsize=(10, 4))
-    shap.plots.waterfall(shap_values[0], max_display=10)
+    shap.plots.waterfall(shap_values[0,:,0], max_display=10)
     st.pyplot(fig)
     # top feature 
-    top_feature = pd.Series(shap_values[0].values, index=shap_values[0].feature_names).abs().idxmax()
+    top_feature = pd.Series(shap_values[0,:,0].values, index=shap_values[0].feature_names).abs().idxmax()
     st.info(f"**Business Insight:** The most influential factor in this decision was **{top_feature}**.")
 
 # Streamlit UI
